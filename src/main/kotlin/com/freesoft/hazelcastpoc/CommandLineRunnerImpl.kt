7934cd9ba06ddbf14538d63f4833958fccb9a4d0
bogdan.dina <bogdan.dina@orange.com>
2019-09-17 21:50:42 +0300

    List and Set example

--- src/main/kotlin/com/freesoft/hazelcastpoc/CommandLineRunnerImpl.kt
+++ src/main/kotlin/com/freesoft/hazelcastpoc/CommandLineRunnerImpl.kt
@@ -16,6 +16,18 @@ class CommandLineRunnerImpl : CommandLineRunner {
         capitals.put("RO", "Bucharest")
         capitals.put("IT", "Rome")
 
+        val cities = hazelcast.getSet<String>("cities")
+        cities.addAll(capitals.values)
+        cities.add("London")
+        cities.add("Rome")
+        cities.add("New York")
+
+        val countries = hazelcast.getList<String>("countries")
+        countries.addAll(capitals.keys)
+        countries.add("CA")
+        countries.add("DE")
+        countries.add("GB")
+
         System.err.println("Known capital cities: ${capitals.size}")
         System.err.println("Capital city of GB: ${capitals.get("GB")}")
     }
