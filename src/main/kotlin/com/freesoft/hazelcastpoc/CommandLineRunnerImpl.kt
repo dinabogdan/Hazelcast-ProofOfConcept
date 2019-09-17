4e83ce58ae1d8b0540f71c929ccf1ef75a08b0bf
bogdan.dina <bogdan.dina@orange.com>
2019-09-17 21:55:43 +0300

    BlockinQueue example

--- src/main/kotlin/com/freesoft/hazelcastpoc/CommandLineRunnerImpl.kt
+++ src/main/kotlin/com/freesoft/hazelcastpoc/CommandLineRunnerImpl.kt
@@ -4,31 +4,31 @@ import com.hazelcast.core.Hazelcast
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 
-@Component
-class CommandLineRunnerImpl : CommandLineRunner {
-    override fun run(vararg args: String?) {
-        val hazelcast = Hazelcast.newHazelcastInstance()
-
-        val capitals = hazelcast.getMap<String, String>("capitals")
-        capitals.put("GB", "London")
-        capitals.put("FR", "Paris")
-        capitals.put("US", "Washington DC")
-        capitals.put("RO", "Bucharest")
-        capitals.put("IT", "Rome")
-
-        val cities = hazelcast.getSet<String>("cities")
-        cities.addAll(capitals.values)
-        cities.add("London")
-        cities.add("Rome")
-        cities.add("New York")
-
-        val countries = hazelcast.getList<String>("countries")
-        countries.addAll(capitals.keys)
-        countries.add("CA")
-        countries.add("DE")
-        countries.add("GB")
-
-        System.err.println("Known capital cities: ${capitals.size}")
-        System.err.println("Capital city of GB: ${capitals.get("GB")}")
-    }
-}
+//@Component
+//class CommandLineRunnerImpl : CommandLineRunner {
+//    override fun run(vararg args: String?) {
+//        val hazelcast = Hazelcast.newHazelcastInstance()
+//
+//        val capitals = hazelcast.getMap<String, String>("capitals")
+//        capitals.put("GB", "London")
+//        capitals.put("FR", "Paris")
+//        capitals.put("US", "Washington DC")
+//        capitals.put("RO", "Bucharest")
+//        capitals.put("IT", "Rome")
+//
+//        val cities = hazelcast.getSet<String>("cities")
+//        cities.addAll(capitals.values)
+//        cities.add("London")
+//        cities.add("Rome")
+//        cities.add("New York")
+//
+//        val countries = hazelcast.getList<String>("countries")
+//        countries.addAll(capitals.keys)
+//        countries.add("CA")
+//        countries.add("DE")
+//        countries.add("GB")
+//
+//        System.err.println("Known capital cities: ${capitals.size}")
+//        System.err.println("Capital city of GB: ${capitals.get("GB")}")
+//    }
+//}
