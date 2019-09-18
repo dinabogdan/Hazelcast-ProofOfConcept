2d4c3549e7c65f14a6cbae54090e1619664293a9
bogdan.dina <bogdan.dina@orange.com>
2019-09-18 14:00:11 +0300

    SqlPredicates over imap

--- src/main/kotlin/com/freesoft/hazelcastpoc/MultiMapCommandLineRunner.kt
+++ src/main/kotlin/com/freesoft/hazelcastpoc/MultiMapCommandLineRunner.kt
@@ -1,42 +1,42 @@
-package com.freesoft.hazelcastpoc
-
-import com.hazelcast.core.Hazelcast
-import org.springframework.boot.CommandLineRunner
-import org.springframework.stereotype.Component
-
-@Component
-class MultiMapCommandLineRunner : CommandLineRunner {
-    override fun run(vararg args: String?) {
-        val hazelcastInstance = Hazelcast.newHazelcastInstance()
-
-        val manualCities = hazelcastInstance.getMap<String, List<String>>("manualCities")
-        var gbCities = mutableListOf<String>()
-        manualCities["GB"] = gbCities
-
-        gbCities = manualCities["GB"] as MutableList<String>
-        gbCities.add("London")
-        manualCities["GB"] = gbCities
-
-        gbCities = manualCities["GB"] as MutableList<String>
-        gbCities.add("Liverpool")
-        manualCities["GB"] = gbCities
-
-        var frCities = mutableListOf<String>()
-        manualCities["FR"] = frCities
-
-        frCities = manualCities["FR"] as MutableList<String>
-        frCities.add("Paris")
-        manualCities["FR"] = frCities
-
-        System.err.println(String.format("Manual: GB=${manualCities["GB"]}, FR=${manualCities["FR"]}"))
-
-        val multiMapCities = hazelcastInstance.getMultiMap<String, String>("multiMapCities")
-
-        multiMapCities.put("GB", "London")
-        multiMapCities.put("GB", "Liverpool")
-        multiMapCities.put("FR", "Paris")
-
-        System.err.println(String.format("MultiMap: GB=${multiMapCities["GB"]}, FR=${multiMapCities["FR"]}"))
-
-    }
-}
+//package com.freesoft.hazelcastpoc
+//
+//import com.hazelcast.core.Hazelcast
+//import org.springframework.boot.CommandLineRunner
+//import org.springframework.stereotype.Component
+//
+//@Component
+//class MultiMapCommandLineRunner : CommandLineRunner {
+//    override fun run(vararg args: String?) {
+//        val hazelcastInstance = Hazelcast.newHazelcastInstance()
+//
+//        val manualCities = hazelcastInstance.getMap<String, List<String>>("manualCities")
+//        var gbCities = mutableListOf<String>()
+//        manualCities["GB"] = gbCities
+//
+//        gbCities = manualCities["GB"] as MutableList<String>
+//        gbCities.add("London")
+//        manualCities["GB"] = gbCities
+//
+//        gbCities = manualCities["GB"] as MutableList<String>
+//        gbCities.add("Liverpool")
+//        manualCities["GB"] = gbCities
+//
+//        var frCities = mutableListOf<String>()
+//        manualCities["FR"] = frCities
+//
+//        frCities = manualCities["FR"] as MutableList<String>
+//        frCities.add("Paris")
+//        manualCities["FR"] = frCities
+//
+//        System.err.println(String.format("Manual: GB=${manualCities["GB"]}, FR=${manualCities["FR"]}"))
+//
+//        val multiMapCities = hazelcastInstance.getMultiMap<String, String>("multiMapCities")
+//
+//        multiMapCities.put("GB", "London")
+//        multiMapCities.put("GB", "Liverpool")
+//        multiMapCities.put("FR", "Paris")
+//
+//        System.err.println(String.format("MultiMap: GB=${multiMapCities["GB"]}, FR=${multiMapCities["FR"]}"))
+//
+//    }
+//}
