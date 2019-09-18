127f5cb3c999ade7e452ecff9134cc62ae7fe111
bogdan.dina <bogdan.dina@orange.com>
2019-09-18 14:01:47 +0300

    sql predicates over imap

---
+++ src/main/kotlin/com/freesoft/hazelcastpoc/City.kt
@@ -0,0 +1,31 @@
+package com.freesoft.hazelcastpoc
+
+import java.io.Serializable
+
+data class City(val name: String,
+                val country: String,
+                val population: Int) : Serializable, Comparable<City> {
+    override fun compareTo(other: City): Int {
+        return population - other.population
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as City
+
+        if (this.country != other.country) return false
+        if (this.name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + country.hashCode()
+        return result
+    }
+
+
+}
